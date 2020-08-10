53b485dc5b85ed51e278bb9d3fae1a5ce02e8d8e
Yanqc <906763115@qq.com>
2020-08-10 18:34:53 +0800

    菜单组件demo提交

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 // 引入路由
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
-} from "react-router-dom";
+} from 'react-router-dom'
 // 引入ant样式
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.css'
 // 引入Provider组件，主要是为子组件和store做一个中间传值的功能
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux'
 // 将store引入
-import store from "./store";
+import store from './store'
 
 // 引入充值样式
-import '@/assets/css/reset.css';
-
+import '@/assets/css/reset.css'
 
 //引入组件
-import Login from "./pages/login"; //登陆
-import Main from "./pages/main"; //main
-import Home from "./pages/home"; //首页
+import Login from './pages/login' //登陆
+import Main from './pages/main' //main
+import Home from './pages/home' //首页
 
 class App extends Component {
   render() {
@@ -45,8 +44,8 @@ class App extends Component {
           </Switch>
         </Router>
       </Provider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,24 +1,130 @@
-import React, { Component } from "react";
-import { withRouter, Link } from "react-router-dom";
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 //connet的作用就是与外部的Provide进行连接
-import { connect } from "react-redux";
+import { connect } from 'react-redux'
+import './less/main.less'
+import { Menu, Switch, Divider } from 'antd'
+import {
+  MailOutlined,
+  CalendarOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+  LinkOutlined,
+} from '@ant-design/icons'
+
+const { SubMenu } = Menu
 
 class Main extends Component {
+  state = {
+    mode: 'inline',
+    theme: 'light',
+    menuList: [
+      {
+        key: '1',
+        name: 'menu1',
+        icon:MailOutlined
+      },
+      {
+        key: '2',
+        name: 'menu1',
+        icon:AppstoreOutlined
+      },
+      {
+        key: '3',
+        name: 'menu1',
+        icon:CalendarOutlined
+      },
+      {
+        key: '4',
+        name: 'menu1',
+        icon:SettingOutlined
+      },
+      {
+        key: '5',
+        name: 'menu1',
+        icon:LinkOutlined
+      },
+      {
+        key: '6',
+        name: 'menu1',
+        icon:MailOutlined,
+        children: [
+          {
+            key: '7',
+            name: 'menu7',
+            icon:AppstoreOutlined
+          },
+          {
+            key: '8',
+            name: 'menu8',
+            icon:CalendarOutlined
+          },
+          {
+            key: '9',
+            name: 'menu9',
+            icon:SettingOutlined
+          },
+        ],
+      },
+    ],
+  }
+
+  changeMode = (value) => {
+    this.setState({
+      mode: value ? 'vertical' : 'inline',
+    })
+  }
+
+  changeTheme = (value) => {
+    this.setState({
+      theme: value ? 'dark' : 'light',
+    })
+  }
+
   render() {
     return (
-      <div>
-        Main
-        <Link to="/main/home">home</Link>
-        {this.props.children}
-      </div>
-    );
+      <>
+        <Switch onChange={this.changeMode} /> Change Mode
+        <Divider type="vertical" />
+        <Switch onChange={this.changeTheme} /> Change Style
+        <br />
+        <br />
+        <Menu
+          style={{ width: 256 }}
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode={this.state.mode}
+          theme={this.state.theme}
+        >
+          {this.state.menuList.map((item) =>
+            item.children ? (
+              <SubMenu
+                key={item.key}
+                icon={<AppstoreOutlined />}
+                title={item.name}
+              >
+                {item.children.map((inner) => (
+                  <Menu.Item key={inner.key} icon={<MailOutlined />}>
+                    {inner.name}
+                  </Menu.Item>
+                ))}
+              </SubMenu>
+            ) : (
+              <Menu.Item key={item.key} icon={<MailOutlined />}>
+                {item.name}
+              </Menu.Item>
+            )
+          )}
+        </Menu>
+      </>
+    )
   }
 }
 
 // state--store内的state
-const mapStateToProps = state => {};
+const mapStateToProps = (state) => {}
 
 // dispatch--调用store的方法，store.dispatch
-const mapDispathToProps = dispatch => {};
+const mapDispathToProps = (dispatch) => {}
 
-export default connect(mapStateToProps, mapDispathToProps)(withRouter(Main));
+export default connect(mapStateToProps, mapDispathToProps)(withRouter(Main))
